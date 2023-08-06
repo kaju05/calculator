54856b5ca7f183914a921c40ebbf93aacce44121
kanika <kanika150405@hotmail.com>
2023-08-07 02:45:30 +0530

    first commit of calculator'

---
+++ main.js
@@ -0,0 +1,30 @@
+let btns=document.querySelectorAll('button');
+let inp=document.querySelector('input');
+inp.style.color='white';
+inp.value="";
+function audio(){
+    let audio=new Audio("audio.wav");
+    audio.play();
+}
+for(let btn of btns){
+    btn.style.backgroundColor='white';
+    btn.addEventListener('click',function(e){
+        audio();
+        let btntxt=e.target.innerText;
+        if(btntxt==='C'){
+            inp.value='';
+        }
+        else if(btntxt==='='){
+            try{
+                
+                inp.value=eval(inp.value);
+            }
+            catch(err){
+                inp.value='invalid';
+            }
+        }
+        else{
+            inp.value+=e.target.innerText;
+        }
+    })
+}
